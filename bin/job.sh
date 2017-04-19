4e6a251058d1179c369501f5ef515247ede7ce32
Severin Räz <raez@puzzle.ch>
2017-04-19 15:38:44 +0200

    Do not dump via stdout, this fails for some databases.

--- bin/job.sh
+++ bin/job.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 DATE=$(date +%Y-%m-%d-%H-%M)
 export PGPASSWORD=$POSTGRESQL_PASSWORD
-dump=$(pg_dump --username=$POSTGRESQL_USER --host=$POSTGRESQL_SERVICE_HOST --port=$POSTGRESQL_SERVICE_PORT $POSTGRESQL_DATABASE)
+pg_dump --username=$POSTGRESQL_USER --host=$POSTGRESQL_SERVICE_HOST --port=$POSTGRESQL_SERVICE_PORT $POSTGRESQL_DATABASE > /tmp/dump.sql
 
 if [ $? -ne 0 ]; then
     echo "db-dump not successful: ${DATE}"
+    rm -f /tmp/dump.sql
     exit 1
 fi
 
-printf '%s' "$dump" | gzip > $BACKUP_DATA_DIR/dump-${DATE}.sql.gz
+gzip /tmp/dump.sql
 
 if [ $? -eq 0 ]; then
+    mv /tmp/dump.sql.gz $BACKUP_DATA_DIR/dump-${DATE}.sql.gz
     echo "backup created: ${DATE}"
 else
     echo "backup not successful: ${DATE}"
+    rm -f /tmp/dump.sql.gz /tmp/dump.sql
     exit 1
 fi
 
